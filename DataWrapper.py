68b1e19f6cfe6a219964a32abd2cd140ca4f08c5
Thomas Langerak <thomas.langerak@inf.ethz.ch>
2019-04-25 12:05:54 +0200

    made it work on CUDA and non-CUDA installations. CUDA is 10x faster.

--- DataWrapper.py
+++ DataWrapper.py
@@ -6,9 +6,11 @@ import torch
 
 
 class DataWrapper(Dataset):
-    def __init__(self, input_dir, target_dir):
+    def __init__(self, input_dir, target_dir, cuda_available):
         self.input_dir = input_dir
         self.target_dir = target_dir
+        self.cuda_available = cuda_available
+        print(self.cuda_available)
         assert len([name for name in os.listdir(self.input_dir)]) == len([name for name in os.listdir(
             self.target_dir)]), "Input and target directory dont have the same number of entries"
 
@@ -16,24 +18,32 @@ class DataWrapper(Dataset):
         return len([name for name in os.listdir(self.input_dir)])
 
     def __getitem__(self, idx):
-        def toTensorRGB(image):
+        def toTensorRGB(self, image):
             # swap color axis because
             # numpy image: H x W x C
             # torch image: C X H X W
             image = image.transpose((2, 0, 1))
-            torch_image = torch.from_numpy(image).type(torch.FloatTensor)
+            if self.cuda_available:
+                torch_image = torch.from_numpy(image).type(torch.FloatTensor).cuda()
+            else:
+                torch_image = torch.from_numpy(image).type(torch.FloatTensor)
             return torch_image
 
-        def toTensorBW(image):
+        def toTensorBW(self, image):
             torch_image = torch.from_numpy(image).view(400, 400, 1)
             torch_image = torch_image.permute((2, 0, 1))
-            return torch_image.type(torch.FloatTensor)
+
+            if self.cuda_available:
+                torch_image = torch_image.type(torch.FloatTensor).cuda()
+            else:
+                torch_image = torch_image.type(torch.FloatTensor)
+            return torch_image
 
         input_img_name = os.path.join(self.input_dir, str(idx).zfill(5) + '.png')
         input_image = io.imread(input_img_name)
         target_img_name = os.path.join(self.target_dir, str(idx).zfill(5) + '.png')
         target_image = io.imread(target_img_name)
-        sample = {'input': toTensorRGB(input_image), 'target': toTensorBW(target_image)}
+        sample = {'input': toTensorRGB(self, input_image), 'target': toTensorBW(self, target_image)}
         return sample
 
 
--- train.py
+++ train.py
@@ -16,7 +16,7 @@ torch.manual_seed(seed)
 input_dir = 'train_augmented/input/'
 target_dir = 'train_augmented/target/'
 
-data = DataWrapper(input_dir, target_dir)
+data = DataWrapper(input_dir, target_dir, torch.cuda.is_available())
 
 # generate indices for creating train, eval and test set.
 indices = range(len(data))
@@ -28,16 +28,16 @@ test_indices = indices_3
 assert len(training_indices) + len(test_indices) + len(eval_indices) == len(data), "Not all data is used!"
 
 # create batches, shuffle needs to be false because we use the sampler.
-training_data = DataLoader(data, shuffle=False, batch_size=10, num_workers=4,
-                           sampler=SubsetRandomSampler(training_indices))
-eval_data = DataLoader(data, shuffle=False, batch_size=1, num_workers=4, sampler=SubsetRandomSampler(eval_indices))
-test_data = DataLoader(data, shuffle=False, batch_size=1, num_workers=4, sampler=SubsetRandomSampler(test_indices))
+training_data = DataLoader(data, shuffle=False, batch_size=10, sampler=SubsetRandomSampler(training_indices))
+eval_data = DataLoader(data, shuffle=False, batch_size=1, sampler=SubsetRandomSampler(eval_indices))
+test_data = DataLoader(data, shuffle=False, batch_size=1, sampler=SubsetRandomSampler(test_indices))
 
 model = SimpleCNN()
-if print(torch.cuda.is_available()) == False:
-    print("CUDA unavailable, using CPU!")
-else:
+if torch.cuda.is_available():
     model.cuda()
+else:
+    print("CUDA unavailable, using CPU!")
+
 
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-2, weight_decay=0.0)
