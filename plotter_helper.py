a9572217c48829b15f0ab3ed0d4c538741e51a7e
Valentin Anklin <valentin@ankl.in>
2019-04-26 15:31:40 +0200

    Added overlay and side by side

--- plotter_helper.py
+++ plotter_helper.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+import matplotlib.image as mpimg
 
 
 def evaluation_side_by_side_plot(inputs, outputs, groundtruth, save=False, save_name=""):
@@ -35,3 +38,47 @@ def evaluation_side_by_side_plot_np(inputs, outputs, groundtruth, save=False, sa
     else:
         fig.savefig(save_name)
     plt.close(fig)
+
+
+def img_float_to_uint8(img):
+    rimg = img - np.min(img)
+    rimg = (rimg / np.max(rimg) * 255).round().astype(np.uint8)
+    return rimg
+
+
+def make_img_overlay(img, predicted_img):
+    w = img.shape[0]
+    h = img.shape[1]
+    color_mask = np.zeros((w, h, 3), dtype=np.uint8)
+    color_mask[:, :, 0] = 255 * predicted_img
+    color_mask[:, :, 1] = 165 * predicted_img
+
+    img8 = img_float_to_uint8(img)
+    background = Image.fromarray(img8, 'RGB').convert("RGBA")
+    overlay = Image.fromarray(color_mask, 'RGB').convert("RGBA")
+    img9 = img_float_to_uint8(predicted_img)
+    alpha = Image.fromarray(255 - img9, "L")
+
+    blended_overlay = Image.blend(background, overlay, 0.7)
+    new_img = Image.composite(background, blended_overlay, alpha)
+    return new_img
+
+
+def overlay_side_by_side(img, ground_truth, prediction, save=False, save_name="plot.png"):
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1, 2, 1)
+    ax2 = fig.add_subplot(1, 2, 2)
+    ax1.imshow(make_img_overlay(img, ground_truth))
+    ax1.set_title("Ground truth")
+    ax2.imshow(make_img_overlay(img, prediction))
+    ax2.set_title("Prediction")
+    if not save:
+        plt.show()
+    else:
+        fig.savefig(save_name)
+    plt.close(fig)
+
+if __name__ == "__main__":
+    image = mpimg.imread("training/images/satImage_001.png")
+    ground_truth = mpimg.imread("training/target/satImage_001.png")
+    overlay_image = overlay_side_by_side(image, ground_truth, ground_truth)
