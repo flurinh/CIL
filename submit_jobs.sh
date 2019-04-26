e1cb0c34a071948a4f78577b9e0b0740ed3b1668
Thomas Langerak <thomas.langerak@inf.ethz.ch>
2019-04-26 13:44:40 +0200

    reduced n_gpus

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -8,7 +8,7 @@ for lr in ${LearningRates[@]}; do
    for bs in ${BatchSizes[@]}; do
         for opt in ${Optimizers[@]}; do
               name="lr_${lr}_bs_${bs}_opt_${opt}"
-              bsub -n 4 -W 25:00 -R "rusage[mem=4096, ngpus_excl_p=3]" python train.py --lr $lr -b $bs -e 50 --op $opt --log $name
+              bsub -n 4 -W 25:00 -R "rusage[mem=4096, ngpus_excl_p=2]" python train.py --lr $lr -b $bs -e 50 --op $opt --log $name
               sleep 1
         done
    done
