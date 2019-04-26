eb8bb87b81a0ac3c41202cd7ae49264ff4615f53
Thomas Langerak <thomas.langerak@inf.ethz.ch>
2019-04-26 20:02:48 +0200

    testing R2Attu

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -9,7 +9,7 @@ for lr in ${LearningRates[@]}; do
    for bs in ${BatchSizes[@]}; do
         for opt in ${Optimizers[@]}; do
             for ts in ${TrainingSet[@]}; do
-              name="lr_${lr}_bs_${bs}_opt_${opt}"
+              name="lr_${lr}_bs_${bs}_opt_${opt}_R2AttU_set${ts}"
               bsub -n 4 -W 25:00 -R "rusage[mem=4096, ngpus_excl_p=2]" python train.py --lr $lr -b $bs -e 50 --op $opt -d $ts --log $name
               sleep 1
               done
