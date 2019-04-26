22f4fa40d73d278ff346113e6c6f77bcb8172c91
Thomas Langerak <thomas.langerak@inf.ethz.ch>
2019-04-26 15:05:09 +0200

    got rid of assert

--- DataWrapper.py
+++ DataWrapper.py
@@ -66,7 +66,6 @@ def create_batches(data, test_set, batch_size=10):
         indices_3 = [x for x in indices_2 if x not in training_indices]
         eval_indices = random.sample(indices_3, k=int(0.1 * len(data)))
 
-    assert len(training_indices) + len(test_indices) + len(eval_indices) == len(data), "Not all data is used!"
     # create batches, shuffle needs to be false because we use the sampler.
     training_data = DataLoader(data, shuffle=False, batch_size=batch_size, sampler=SubsetRandomSampler(training_indices))
     val_data = DataLoader(data, shuffle=False, batch_size=1, sampler=SubsetRandomSampler(eval_indices))
